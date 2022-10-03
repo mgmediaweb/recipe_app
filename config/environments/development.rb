01ca755a9218ba26cf22fcfccb34320cc186f2c7
darikmohammed <r.m.darik@gmail.com>
2022-10-03 23:45:24 +0300

    Finish Login and registaration with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,10 +18,10 @@ Rails.application.configure do
   config.server_timing = true
 
   # Default url options in your environments files.
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '25b38fd13d9f0c5fc886e3dc618b9fc74fc93d9ecef532af11eef92f4bbb752da7170fe12a8dbb4d924df14bd54fa4406fd2d0c2b81794ebdf0df1f679cbe1fd'
-
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
