cfd056040565fe90c7d38fe271aef10b133e9a41
darikmohammed <r.m.darik@gmail.com>
2022-10-05 18:16:01 +0300

    Update controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
-  before_action :authenticate_user!
 
   # GET /recipes or /recipes.json
   def index
